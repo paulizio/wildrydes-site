a4996d1894873ac3e078fb872cb59715e75b533d
paulizio <pauli.nordman@hotmail.com>
2021-12-05 09:28:34 +0200

    added invokeUrl

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: 'eu-west-1_72VkvLIHF', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2j5i4n49lmog8bbiegj48o2k6u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-1' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
-};
+  cognito: {
+    userPoolId: 'eu-west-1_72VkvLIHF', // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: '2j5i4n49lmog8bbiegj48o2k6u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: 'eu-west-1' // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: 'https://nrcz5x1vhj.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  }
+}
